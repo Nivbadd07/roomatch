3cf046795f22613856e2e85c6bcd23f29c078811
Amit Geller <amitge11@gmail.com>
2025-05-21 19:32:28 +0300

    sdsdsd

--- models.js
+++ models.js
@@ -1,6 +1,8 @@
-import dotenv from 'dotenv';
-dotenv.config();
 import { Sequelize, DataTypes } from 'sequelize';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL environment variable is not set. Please check your .env file or Cloud Run environment variables.');
+}
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
